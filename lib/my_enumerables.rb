54c582f4b2552382a273c799af218df66645f1cd
Necromancy501 <94140246+Necromancy501@users.noreply.github.com.>
2024-12-23 09:30:07 +0800

    Deploy solution

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,62 @@
 module Enumerable
-  # Your code goes here
+  def my_all?
+    my_each do |element|
+      return false unless yield(element)
+    end
+    true  
+  end
+
+  def my_any?
+    my_each do |element|
+      return true if yield(element)
+    end
+    false
+  end
+
+  def my_count
+    index = 0
+    block_given? ? my_each {|element| index+=1 if yield(element)} : my_each {|element| index+=1}
+    index
+  end
+
+  def my_each_with_index
+    index=0
+    my_each do |element|
+      yield(element, index)
+      index+=1
+    end
+    self
+  end
+
+  def my_map
+    my_each_with_index do |element, index|
+      self[index] = yield(element)
+    end
+  end
+
+  def my_none? 
+    my_each do |element|
+      return false if yield(element)
+    end
+    true
+  end
+
+  def my_select
+    result = []
+    my_each do |element|
+      result.push(element) if yield element
+    end
+    result
+  end
+
+  def my_inject initial_value
+    my_each_with_index do |element, index|
+      initial_value = yield initial_value, element
+    end
+    initial_value
+
+  end
+
 end
 
 # You will first have to define my_each
@@ -7,5 +64,13 @@ end
 # your enumerable module will have access
 # to this method
 class Array
-  # Define my_each here
+  def my_each
+    length = self.length
+    index = 0
+    length.times do
+      yield self[index]
+      index+=1
+    end
+    self
+  end
 end
